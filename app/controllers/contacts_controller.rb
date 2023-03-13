ad12f9d03e5a65f513bc5c21c008d78cfafe2f6c
Jamie Crone <jamiecrone@gmail.com>
2023-03-13 10:57:28 -0700

    Name Contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -65,6 +65,6 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :company, :address, :phone, :notes, :email)
+      params.require(:contact).permit(:name, :company, :address, :phone, :notes, :email)
     end
 end
